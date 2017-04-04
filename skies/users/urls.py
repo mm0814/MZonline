b1be47c411fe381695c0e441ce2ff4b113b0add0
mm_test <yezi0418@qq.com>
2017-04-04 20:48:23 +0800

    users all .py is the first commit content:" module"

---
+++ skies/users/urls.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+"""
+create on 2017/4/4
+@author:  已开
+功能描述
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from users import views
+
+urlpatterns = [
+    # url(r'^admin/', admin.site.urls),
+    # url(r'^todo/$',views.todo,name='index'),
+
+
+]
